2541d0bedafbb1c81915bbb5e744820e290e6142
Kate de Leon <138152818+keirisa@users.noreply.github.com>
2024-11-01 18:50:23 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./views/**/*.ejs'], // updateed to all .ejs files in the views folder
+  content: ['./views/*/.ejs'], // updateed to all .ejs files in the views folder
   theme: { extend: {}, },
   plugins: [require('@tailwindcss/typography'), require('daisyui')],
   daisyui: { themes: ['cupcake'], },
